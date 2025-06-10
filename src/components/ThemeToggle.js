b245cbe7c0117234c3e50d3d9bbaf3428ae2fb34
Daniel Lopes <danieltayt-sohn@hotmail.com>
2025-06-09 21:58:41 -0300

    perfumaria

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
+import Logo from '../assets/123.png';
 
 function ThemeToggle() {
   const [dark, setDark] = useState(false);
 
   const handleToggle = () => {
     setDark(!dark);
-    document.body.style.backgroundColor = !dark ? '#222' : '#fff';
-    document.body.style.color = !dark ? '#fff' : '#222';
+    document.body.style.backgroundColor = !dark ? '#000' : '#fff';
+    document.body.style.color = !dark ? '#fff' : '#000';
   };
 
   return (
-    <button onClick={handleToggle}>
-      {dark ? 'Tema Claro' : 'Tema Escuro'}
-    </button>
+    <div>
+      <img src={Logo} alt="Imagem do tema" style={{ width: '100px'}} />
+      <button onClick={handleToggle} style={{display: 'flex', margin: ' auto', justifyContent: 'center', alignContent: 'center' }}>
+        {dark ? 'Tema Claro' : 'Tema Escuro'}
+      </button>
+
+    </div>
   );
 }
 
